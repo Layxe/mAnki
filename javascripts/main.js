bf6f553dc9a36ae248ed6f569e8f5baec5957b36
Layxe <layxe.alex@gmail.com>
2020-11-26 12:38:52 +0100

    implemented mathjax rendering and clipboard

---
+++ javascripts/main.js
@@ -0,0 +1,42 @@
+let input   = document.getElementById('input')
+let output  = document.getElementById('output')
+let display = document.getElementById('display')
+let copyInput = document.getElementById('copy-input')
+let clipboardStatus = document.getElementById('clipboard-status')
+
+let updateDisplay = () => {
+
+    let code = input.value
+
+    // Render the code
+    display.innerHTML = `\\[${code}\\]`
+    MathJax.typeset();
+    output.innerHTML = `\\[${code}\\]`
+
+    clipboardStatus.innerHTML = 
+            `<span class="icon has-text-danger">
+                <i class="fas fa-spinner fa-pulse"></i>
+            </span>`
+    
+    // Write mathjax code into the clipboard and display the status
+    navigator.clipboard.writeText(`\\[${code}\\]`)
+        .catch((err) => {
+
+            clipboardStatus.innerHTML = 
+            `<span class="icon has-text-danger">
+                <i class="fas fa-ban"></i>
+            </span>`
+
+        })
+        .then((res) => {
+
+            clipboardStatus.innerHTML = 
+            `<span class="icon has-text-success">
+                <i class="fas fa-check-square"></i>
+            </span>`
+
+        })
+
+}
+
+input.addEventListener('keyup', updateDisplay)
